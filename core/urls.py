14b7279015abb5c2cf17556173e6b939824e9880
Jays-Network <jullian0jones@gmail.com>
2026-01-11 22:22:23 +0000

    Fix infinite recursion loop in urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')), 
+    path('', views.index, name='index'),
+    path('projects/', views.projects, name='projects'),
 ]
