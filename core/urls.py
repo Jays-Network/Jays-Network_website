f18477a62dca384c8dca1a2d94337a25e2dc99fb
Jays-Network <jullian0jones@gmail.com>
2026-01-11 22:14:03 +0000

    Fix config urls and core view imports

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include 
-from core.views import index, projects  # Updated import
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # This line tells Django to look at your core/urls.py file for all other URLs
     path('', include('core.urls')), 
 ]
