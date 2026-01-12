6c8c3c7ff9a929aec6a17fd280dbde5114241984
Jays-Network <jullian0jones@gmail.com>
2026-01-12 21:48:22 +0000

    Replace social row with Uplink Button and create Contact Terminal

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
 ]
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('projects/', views.projects, name='projects'),
-    path('contact/', views.contact, name='contact'), # <--- New Line
-]
